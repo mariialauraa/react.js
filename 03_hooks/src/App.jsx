8c2403a12f67456ed4ef7a3e1c00033e628681b2
Maria Laura <mlauraifg@gmail.com>
2024-06-19 23:26:01 -0300

    useMemo e useCallback

--- 03_hooks/src/App.jsx
+++ 03_hooks/src/App.jsx
@@ -8,6 +8,8 @@ import Contador from './components/Contador'
 import DisplayWindowSize from './components/DisplayWindowSize'
 import Container from './components/Container'
 import PerfilDeUsuario from './components/PerfilDeUsuario'
+import CalculoPesado from './components/CalculoPesado'
+import ContadorCallback from './components/ContadorCallback'
 
 
 function App() {
@@ -16,6 +18,7 @@ function App() {
     {/* 2.1 useEffect */}
     <ExUseEffect />
     <Timer />
+
     {/* 2.2 useContext */}
     {/* aplicações de pequeno e médio porte, que precisam transferir o estado entre componentes */}
     <MeuContextoProvider>
@@ -23,21 +26,33 @@ function App() {
       <ComponenteFilho /> 
       <ValorDoContexto />
     </MeuContextoProvider>
+
     {/* 2.3 useReducer */}
     {/* estudos mais complexos */}
     <Contador />
+
     {/* 2.4 Custom hook */}
     <DisplayWindowSize />
+
     {/* 2.5 Slots e children props */}
     <Container>
       <h1>Título da seção</h1>
       <p>Este é o meu subtítulo</p>
       <Contador />
     </Container>
+
     {/* 2.6 Sicronizar o estado com props */}
     {/* prop => componente => chamada API => resulta em um dado */}
     <PerfilDeUsuario usuarioId={1}/>
     <PerfilDeUsuario usuarioId={2}/>
+
+    {/* 2.7 Hooks que ajudam na performance da aplicação */}
+    {/* useMemo*/}
+    <CalculoPesado numero={5}/>
+    {/* Caso não mude o valor do 'numero' o React salva o valor na memória e não recria a função 'CalculoPesado' */}
+
+    {/* useCallback */}
+    <ContadorCallback />
     </>
   )
 }
