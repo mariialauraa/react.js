8cec2274f3daa96c7ddea6f3461ed2b44e7f8719
Maria Laura <mlauraifg@gmail.com>
2024-06-21 11:15:37 -0300

    Atualizando (UPDATE/PUT) e excluindo (DELETE)

--- 06_api/src/components/PostForm.jsx
+++ 06_api/src/components/PostForm.jsx
@@ -1,44 +1,88 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import axios from 'axios'
 
-const PostForm = ({ onSuccess }) => {
-  const [title, setTitle] = useState("")
-  const [body, setBody] = useState("")
+const PostForm = ({ post, onSuccess }) => {
+  const [title, setTitle] = useState(post?.title || "")
+  const [body, setBody] = useState(post?.body || "")
+
+  // para receber os dados no input qdo for editar um post
+  useEffect(() => {
+    if (post) {
+      setTitle(post.title);
+      setBody(post.body);
+    // limpa a edição quando clica em 'Cancelar edição'
+    } else {
+      setTitle('');
+      setBody('');
+    }
+  }, [post]);
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-
     const newPost = {title, body, userId: 1}
 
     try {
-      const response = await axios.post("https://jsonplaceholder.typicode.com/posts", newPost)
-
-      // adicionar post na lista 
-      onSuccess(response.data, "add")     
+      if(post) {
+        // atualiza post
+        const response = await axios.put(
+          `https://jsonplaceholder.typicode.com/posts/${post.id}`,
+          newPost
+        )
+        onSuccess(response.data, "update") 
+      // se não, apenas adiciona
+      } else {
+        const response = await axios.post(
+          "https://jsonplaceholder.typicode.com/posts", newPost
+        )
+        // adicionar post na lista 
+        onSuccess(response.data, "add") 
+      }
+      // limpa a edição
+      setTitle("") 
+      setBody("")
 
     } catch (error) {
       console.log("Erro ao enviar postagem", error)
     }
   }
 
+  // funcao para deletar
+  const handleDelete = async() => {
+    try {
+      await axios.delete(
+        `https://jsonplaceholder.typicode.com/posts/${post.id}`
+      );
+      onSuccess(post, "delete");
+      // limpa a edição
+      setTitle("") 
+      setBody("")
+
+    } catch (error) {
+      console.error("Erro ao deletar postagem:", error);
+    }
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
-      <input 
-        type="text" 
-        value={title} 
-        placeholder="Digite o título" 
-        onChange={(e) => setTitle(e.target.value)}
-      />
+        <input 
+          type="text" 
+          value={title} 
+          placeholder="Digite o título" 
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </div>
       <div>
-      <textarea 
-        value={body} 
-        placeholder="Digite o conteúdo" 
-        onChange={(e) => setBody(e.target.value)}
-      ></textarea>
+        <textarea 
+          value={body} 
+          placeholder="Digite o conteúdo" 
+          onChange={(e) => setBody(e.target.value)}
+        />
       </div>
       <button type="submit">Enviar</button>
+      {post && (
+        <button type="button" onClick={handleDelete}>Excluir</button>
+      )}
     </form>
   )
 }
--- 06_api/src/components/PostManager.jsx
+++ 06_api/src/components/PostManager.jsx
@@ -4,14 +4,31 @@ import axios from 'axios'
 import PostForm from "./PostForm"
 
 const PostManager = () => {
-    const [posts, setPots] = useState([]) 
+    const [posts, setPosts] = useState([]) 
     const [error, setError] = useState("") 
+
+    // dados do post para edição
+    const [selectedPost, setSelectedPost] = useState(null)
+    const [isEditing, setIsEditing] = useState(false)
 
     // função para algum tipo de operação
     const handleSuccess = (post, operation) => {
-        if(operation === "add") {
-            setPots((currentPosts) => [post, ...currentPosts])
+        if (operation === "add") {
+            setPosts((currentPosts) => [post, ...currentPosts]);
+        } 
+        else if (operation === "update") {
+            setPosts((currentPosts) =>
+              currentPosts.map(
+                (currentPost) => (currentPost.id === post.id ? post : currentPost))
+                // se for o msm ID atualiza, se não retorna o post atual
+            );
+        // operação para exclusão
+        } else if (operation === "delete") {
+            setPosts((currentPosts) => currentPosts.filter((currentPost) => currentPost.id !== post.id));
+            // se o ID for diferente retorna o post, se igual exclui
+            setSelectedPost(null) // retorna ao estado inicial
         }
+        setIsEditing(false) // retorna ao estado inicial
     }
 
     useEffect(() => {
@@ -21,7 +38,7 @@ const PostManager = () => {
                     "https://jsonplaceholder.typicode.com/posts"
                 )
                 // pega os 5 primeiros itens
-                setPots(response.data.slice(0, 5))
+                setPosts(response.data.slice(0, 5))
 
             } catch (error) {
                setError(error.message) 
@@ -31,19 +48,37 @@ const PostManager = () => {
         axiosPosts()
     }, [])
 
+    // função para edição
+    const handleEdit = (post) => {
+        setSelectedPost(post)
+        setIsEditing(true)
+    }
+
+    // retorna ao estado inicial do useState
+    const handleCancelEdit = () => {
+        setSelectedPost(null)
+        setIsEditing(false)
+    }
+
   return (
     <div>
         <h1>Gerenciar Posts</h1>
-        <PostForm onSuccess={handleSuccess} />
+        <PostForm 
+            post={isEditing ? selectedPost : null} 
+            onSuccess={handleSuccess} 
+        />
+        {isEditing && <button onClick={handleCancelEdit}>Cancelar edição</button>}
+
         <h2>Postagens</h2>
         {error ? (<p>Erro: {error}</p>) :
-        posts.map((post) => (
-            <div key={post.id}>
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
-                <button>Editar</button>
-            </div>
-        ))}
+            posts.map((post) => (
+                <div key={post.id}>
+                    <h3>{post.title}</h3>
+                    <p>{post.body}</p>
+                    <button onClick={() => handleEdit(post)}>Editar</button>
+                </div>
+            ))
+        }
     </div>
   )
 }
